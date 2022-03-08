058e6f843fb5486338fbd64ac7750f85ea34d5f9
Josiah Luna <josiah.luna@live.com>
2022-03-07 21:14:31 -0800

    Updated backprop

--- main.py
+++ main.py
@@ -7,16 +7,11 @@ from nnfs.datasets import spiral_data
 #X, y = spiral_data(100, 3)
 #np.random.shuffle(X)
 
-def dSSRdW():
-    pass
-
-
-class main():
+if __name__ == "__main__"
     def __init__(self) -> None:
         #This block defines the layers and their shapes.
         #dense1 is matrix of shape ([ [1], [2], [3] ], [ [1], [2], [3] ])
         dense1 = Node.Layer(2,3)
-        #print(dense1.biases,"hey \n", dense1.weights)
         activation1 = Node.Activ_ReLU()
 
         dense2 = Node.Layer(3,4)
@@ -26,7 +21,7 @@ class main():
         activation3 = Node.Activ_SoftMax()
 
         #This block runs the layers and their activation functions.
-        #Also establishes the relationship between the layers.
+        #Also establishes a relationship between the layers.
         dense1.forward(Node.X)
         activation1.forward(dense1.output, dense1.biases)
 
@@ -36,8 +31,11 @@ class main():
         dense3.forward(activation2.output)
         activation3.forward(dense3.output, dense3.biases)
 
+        #Following block is for training the weights and biases.
+        err = Backprop.linearErr(Node.OneHot(),activation3.output)
 
+        #General format: dSSR/dPredicted * dPredicted/dy * dy/dx * dx/dw = Sum(-2(Obsv - Pred)) * w * omega deriv * Input or 1
+        adj_wt1 = Backprop.derivSSRdW(Backprop.linearErr()) * dense1.weights * Node.Find_Omega_Derv(err) * Node.X
+        adj_b1 = Backprop.derivSSRdW(Backprop.linearErr()) * dense1.weights * Node.Find_Omega_Derv(err) * 1
 
-        print(activation3.output)
-
-main()
+        adj_wt2 = Backprop.derivSSRdW(Backprop.linearErr()) * dense2.weights * Node.Find_Omega_Derv(err) * Node.X
